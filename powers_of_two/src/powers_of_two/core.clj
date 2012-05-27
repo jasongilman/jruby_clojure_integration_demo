f3e5bd878f465b52ecc4a04f024707858b33a0af
Jason Gilman <jason@element84.com>
2012-05-27 19:10:27 -0400

    Implemented two powers of two functions and added tests.

--- powers_of_two/src/powers_of_two/core.clj
+++ powers_of_two/src/powers_of_two/core.clj
@@ -1,1 +1,11 @@
 (ns powers_of_two.core)
+
+(defn powers_of_two
+  ([]
+    (concat [1 2 4] (powers_of_two 4N)))
+  ([last_value]
+    (let [next_value (* last_value 2N)]
+      (lazy-seq (cons next_value (powers_of_two next_value))))))
+
+(defn powers_of_two_improved []
+  (iterate (fn [v] (* v 2N)) 1))
--- powers_of_two/test/powers_of_two/test/core.clj
+++ powers_of_two/test/powers_of_two/test/core.clj
@@ -1,6 +1,21 @@
 (ns powers_of_two.test.core
   (:use [powers_of_two.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [clojure.contrib.math :as math]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn test_beginning_of_powers_of_two [f]
+  (is (= [1 2 4 8 16] (take 5 (f)))))
+
+(defn test_very_large_powers_of_two [f]
+  (is (== (math/expt 2 500N) (nth (f) 500N))))
+
+(deftest powers_of_two_test
+  (do
+    (test_beginning_of_powers_of_two powers_of_two)
+    (test_very_large_powers_of_two powers_of_two)))
+
+(deftest powers_of_two_improved_test
+  (do
+    (test_beginning_of_powers_of_two powers_of_two_improved)
+    (test_very_large_powers_of_two powers_of_two_improved)))
+
